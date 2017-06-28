c590d865aeac51c623dde158d3f49de7db25630a
tiagopontoxavier <tiagopontoxavier@gmail.com>
2017-06-28 01:18:24 +0100

    first

---
+++ src/helloworld/base/hello.java
@@ -0,0 +1,10 @@
+package helloworld.base;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello fuckin world");
+	}
+
+}
